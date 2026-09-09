584e38f2197c053d6de319c65fe50c1ca043dd9b
Andrew Jared N. Veracruz <sakurajivc@gmail.com>
2026-09-09 19:52:09 +0800

    Old Project File (For Documentation only.)

---
+++ ICT_2_3Q_Veracruz.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ict_2_3q_veracruz;
+
+/**
+ *
+ * @author MSI
+ */
+public class ICT_2_3Q_Veracruz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
